a158a307bd51b60e5db62bf505b3f6b40d1b0d4f
madu12 <sameeramadushan0812@gmail.com>
2024-12-05 06:34:52 +0530

    json data

--- js/plots.js
+++ js/plots.js
@@ -17,13 +17,15 @@ d3.csv("https://raw.githubusercontent.com/madu12/metro-interstate-traffic-volume
         rain_1h: +d.rain_1h,
         snow_1h: +d.snow_1h,
         clouds_all: +d.clouds_all,
-        date_time: new Date(d.date_time)
+        holiday_indexed: +d.holiday_indexed,
+        date_time: new Date(d.date_time),
     }));
 
     // Initial render
     renderHistogram("traffic_volume");
     renderScatter("temp");
     renderTimeSeries();
+
 
 
     // Update charts on dropdown change
@@ -130,55 +132,46 @@ function renderScatter(xVariable) {
 
 // Function to render the time series plot
 function renderTimeSeries() {
-  // Aggregate data by day
   const aggregatedData = d3.rollup(
       data,
-      v => d3.sum(v, d => d.traffic_volume), // Sum traffic volume
-      d => d3.timeDay(d.date_time) // Group by day
+      v => d3.sum(v, d => d.traffic_volume),
+      d => d3.timeDay(d.date_time)
   );
 
-  // Convert the map to an array of objects for plotting
   const dailyData = Array.from(aggregatedData, ([key, value]) => ({
       date: key,
       traffic_volume: value
   }));
 
-  // Clear the previous plot
   d3.select("#time-series").html("");
 
-  // Set up scales
   const x = d3.scaleUtc()
-      .domain(d3.extent(dailyData, d => d.date)) // X-axis: date range
+      .domain(d3.extent(dailyData, d => d.date))
       .range([margin.left, width - margin.right]);
 
   const y = d3.scaleLinear()
-      .domain([0, d3.max(dailyData, d => d.traffic_volume)]) // Y-axis: traffic volume
+      .domain([0, d3.max(dailyData, d => d.traffic_volume)])
       .nice()
       .range([height - margin.bottom, margin.top]);
 
-  // Line generator
   const line = d3.line()
       .x(d => x(d.date))
       .y(d => y(d.traffic_volume));
 
-  // Create the SVG container
   const svg = d3.select("#time-series")
       .append("svg")
       .attr("width", width)
       .attr("height", height);
 
-  // Add x-axis
   svg.append("g")
       .attr("transform", `translate(0,${height - margin.bottom})`)
       .call(d3.axisBottom(x).ticks(width / 80).tickSizeOuter(0));
 
-  // Add y-axis
   svg.append("g")
       .attr("transform", `translate(${margin.left},0)`)
       .call(d3.axisLeft(y).ticks(height / 40))
       .call(g => g.select(".domain").remove());
 
-  // Add the line path
   svg.append("path")
       .datum(dailyData)
       .attr("fill", "none")
@@ -186,12 +179,10 @@ function renderTimeSeries() {
       .attr("stroke-width", 1.5)
       .attr("d", line);
 
-  // Tooltip container
   const tooltip = d3.select("body").append("div")
       .attr("class", "tooltip")
       .style("opacity", 0);
 
-  // Add mouse events for tooltips
   svg.selectAll("circle")
       .data(dailyData)
       .enter()
@@ -200,7 +191,7 @@ function renderTimeSeries() {
       .attr("cy", d => y(d.traffic_volume))
       .attr("r", 5)
       .attr("fill", "orange")
-      .attr("opacity", 0) // Initially hidden
+      .attr("opacity", 0)
       .on("mouseover", (event, d) => {
           tooltip.transition().duration(200).style("opacity", 0.9);
           tooltip.html(`Date: ${d3.timeFormat("%Y-%m-%d")(d.date)}<br>Traffic Volume: ${d.traffic_volume}`)
